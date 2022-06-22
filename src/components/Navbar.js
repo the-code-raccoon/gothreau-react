ea63bef5a7a98a1a04cdeba9cf3a0207ec0c08b2
otrachea <xtrachea@gmail.com>
2022-06-21 23:36:00 -0400

    Add responsive navbar with hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { Nav, NavMenu, NavLogo, NavItem, Hamburger } from "./Navbar-styles";
+
 export default function Navbar() {
-  return <nav><h1>Gothreau</h1></nav>
+  const [hamburgerActive, setHamburgerActive] = useState(false);
+
+  return (
+    <Nav>
+      <NavLogo>Gothreau</NavLogo>
+      <NavMenu className={hamburgerActive ? "" : "active"}>
+        <NavItem>
+          <a href="#" class="nav-link">
+            About
+          </a>
+        </NavItem>
+        <NavItem>
+          <a href="#" class="nav-link">
+            Music
+          </a>
+        </NavItem>
+        <NavItem>
+          <a href="#" class="nav-link">
+            Contact
+          </a>
+        </NavItem>
+      </NavMenu>
+      <Hamburger
+        className={hamburgerActive ? "active" : ""}
+        onClick={() => {
+          setHamburgerActive((prev) => !prev);
+        }}
+      >
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </Hamburger>
+    </Nav>
+  );
 }
